c75b02a29113a4b5423c6aa63c0eb61f6d8c4a8a
gal0wsky <maciej.gawlowski.biznes@gmail.com>
2023-08-31 09:21:42 +0200

    Setting up the project

---
+++ lib/config/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color.fromARGB(255, 32, 32, 32);
+  static const Color secondary = Color.fromARGB(255, 44, 44, 44);
+  static const Color textPrimary = Color.fromARGB(255, 242, 242, 242);
+  static const Color textSecondary = Color.fromARGB(255, 203, 203, 203);
+  static const Color callToAction = Color.fromARGB(255, 243, 137, 13);
+  static const Color cancel = Color.fromARGB(255, 202, 34, 34);
+}
